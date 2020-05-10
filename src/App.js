43709fb9327afacabe59221c574e5b5eeff29882
Gaith-M <mhdgaith94@gmail.com>
2020-05-11 01:47:30 +0300

    First comment in the react-dev branch

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import logo from "./assets/coronavirus_icon.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Test</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>PandemicsMap</h1>
+        <p>Testing</p>
       </header>
     </div>
   );
